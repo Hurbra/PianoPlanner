2d56709c7f9c15a929c0cd31a2ff5a91871b43c8
joakimstolen <joakimstoelen@hotmail.com>
2019-05-31 11:23:39 +0200

    Create Project v.5

--- Registrere/newProject.js
+++ Registrere/newProject.js
@@ -21,6 +21,24 @@ function addName(){
 }
 
 
+var input = document.getElementById("textBox1");
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+   event.preventDefault();
+   document.getElementById("addProjectName").click();
+     event.currentTarget.value = "";
+  }
+});
+
+var input = document.getElementById("textBox2");
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+   event.preventDefault();
+   document.getElementById("addUserButton").click();
+      event.currentTarget.value = "";
+  }
+});
+
 
 function removeAll1(){
     document.getElementById("listUsers").innerHTML = "";
@@ -29,4 +47,7 @@ function removeAll1(){
 function removeAll(){
     document.getElementById("listedPname").innerHTML = "";
 }
+
+
+//localStorage.setItem("tBox", document.getElementById("textBox1"));
 
