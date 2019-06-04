35dd4219ee3ed099c6bdb9d05f0f2929744e05d5
simonkarlsen <si-karlsen@hotmail.com>
2019-06-04 22:46:27 +0200

    Update

--- Planner/kortDD.js
+++ Planner/kortDD.js
@@ -1,37 +1,5 @@
 
-let addCardButton = document.getElementById("addCardButton");
-
-addCardButton.addEventListener("click", e => {
-	addCard();
-});
-
-
-function addCard(qualifiedName, value) {
-	console.log("I funksjon");
-	let card = document.createElement("li");
-	
-	card.setAttribute("draggable", true);
-	card.className = "card";
-	
-	let drop = document.getElementById("drop1");
-	
-	let nodeP = document.createElement("p");                 // Create a <li> node
-	let input = prompt("Write the title of the card: ");
-	var textNode = document.createTextNode(input);         // Create a text node
-	
-	nodeP.appendChild(textNode);
-	card.appendChild(nodeP);
-	drop.appendChild(card);
-	
-	console.log("Nytt kort opprettet: " + card);
-	
-	dragDrop();
-	
-}
-
-
 dragDrop();
-
 function dragDrop() {
 	
 	let cards = document.getElementsByClassName("card");
@@ -71,29 +39,131 @@ function dragDrop() {
 		
 	}, false);
 	
+	
 	//drop event to allow the element to be dropped into valid targets
 	document.addEventListener('drop', function (e) {
 		//if this element is a drop target, move the item here
 		//then prevent default to allow the action (same as dragover)
 		
-		
+		if(e.target.className === "dropZone") {
+			
 			e.target.appendChild(card);
 			
 			e.preventDefault();
-		
+		}
 		
 	}, false);
 	
 	//dragend event to clean-up after drop or abort
 	//which fires whether or not the drop target was valid
-	document.addEventListener('dragend', function (e) {
+	document.addEventListener('dragend', function () {
 		card = null;
 		
 	}, false);
 	
 }
+//-----
+
+let cardArray = [];
 
 
+let addCardButton = document.getElementById("addCardButton");
+let counter = 0;
+
+addCardButton.addEventListener("click", e => {
+	counter ++;
+	addCard();
+});
+
+//-----
+let buttonRemove = document.getElementById("buttonRemove");
+
+buttonRemove.addEventListener("click", e => {
+	counter --;
+	removeCard();
+});
+//-----
+
+
+
+function addCard(qualifiedName, value) {
+	
+	console.log("I funksjon");
+	
+	let card = document.createElement("li");
+	let drop = document.getElementById("drop1");
+	card.setAttribute("draggable", true);
+	card.className = "card";
+	
+	// Dobbeltklikk på kortet fører til removeCard():
+	//card.setAttribute("ondblclick", "removeCard();");
+	
+	
+	let nodeP = document.createElement("p");
+	nodeP.className = "pCard";
+	let nodeID = document.createElement("p");
+	nodeID.className = "pCard";
+	let input = prompt("Write the title of the card: ");
+	
+	if(input === null) {
+		alert("You have to give the card a title. Try again.");
+		return;
+	}
+	
+	let inputPlusTitle = "Title: " + input;
+	
+	let textNode = document.createTextNode(inputPlusTitle);         // Create a text node
+	
+	let id = "card" + counter;
+	card.id = id;
+	cardArray.push(card + "" + id);
+	
+	let cardNodeID = "ID: " + id;
+	
+	let textNodeID = document.createTextNode(cardNodeID);
+	
+	console.log(card + "" + id);
+	
+	nodeP.appendChild(textNode);
+	card.appendChild(nodeP);
+	nodeID.appendChild(textNodeID);
+	card.appendChild(nodeID);
+	drop.appendChild(card);
+	
+	console.log(counter);
+	console.log("Nytt kort opprettet: " + card);
+	
+	console.log(cardArray);
+	
+	dragDrop();
+	
+}
+
+
+//-----
+
+
+
+function removeCard(e) {
+	
+	let deleteCardID = prompt("Write the ID of the card you want to delete (e.g. --> card1): ").toLowerCase();
+	let deleteOrNot = prompt("Do you want to delete this card?" +"\nWrite yes or no").toLowerCase();
+	
+	let cardIDOfCard = document.getElementById(deleteCardID);
+	let dropParent = cardIDOfCard.parentNode;
+	
+	if (deleteOrNot === "yes") {
+		dropParent.removeChild(cardIDOfCard);
+	}
+	else if(deleteOrNot === null){
+		return;
+	}
+	else {
+		return;
+	}
+	
+	dragDrop();
+}
 
 
 
