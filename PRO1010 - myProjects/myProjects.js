135254aff67372d3da26aa151033617f75f05439
joakimstolen <joakimstoelen@hotmail.com>
2019-06-04 11:52:52 +0200

    myProjectsPage joakimEdit v.1

---
+++ PRO1010 - myProjects/myProjects.js
@@ -0,0 +1,16 @@
+function showPictures(){
+    
+    //Show/hide picture of users that are in the project
+    document.getElementById('user1').style.visibility=    document.getElementById('user1').style.visibility == 'visible'? 'hidden' : 'visible';
+    
+    document.getElementById('user2').style.visibility=    document.getElementById('user2').style.visibility == 'visible'? 'hidden' : 'visible';
+    
+    document.getElementById('user3').style.visibility=    document.getElementById('user3').style.visibility == 'visible'? 'hidden' : 'visible';
+    
+    document.getElementById('user4').style.visibility=    document.getElementById('user4').style.visibility == 'visible'? 'hidden' : 'visible';
+    
+    document.getElementById('user5').style.visibility=    document.getElementById('user5').style.visibility == 'visible'? 'hidden' : 'visible';
+    
+    document.getElementById('user6').style.visibility=    document.getElementById('user6').style.visibility == 'visible'? 'hidden' : 'visible';
+    
+}
