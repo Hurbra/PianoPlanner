ec115f1322ed4e34fd1fc4c870ba028617399108
Wetle Wetlesen <wetlewetlesen@gmail.com>
2019-06-05 15:45:17 +0200

    New logo, new users

--- PRO1010 - myProjects/myProjects.js
+++ PRO1010 - myProjects/myProjects.js
@@ -13,4 +13,18 @@ function showPictures(){
     
     document.getElementById('user6').style.visibility=    document.getElementById('user6').style.visibility == 'visible'? 'hidden' : 'visible';
     
+    document.getElementById('ingrid').style.visibility=    document.getElementById('ingrid').style.visibility == 'visible'? 'hidden' : 'visible';
+    
+    document.getElementById('joakim').style.visibility=    document.getElementById('joakim').style.visibility == 'visible'? 'hidden' : 'visible';
+    
+    document.getElementById('mats').style.visibility=    document.getElementById('mats').style.visibility == 'visible'? 'hidden' : 'visible';
+    
+    document.getElementById('simon').style.visibility=    document.getElementById('simon').style.visibility == 'visible'? 'hidden' : 'visible';
+    
+    document.getElementById('solveig').style.visibility=    document.getElementById('solveig').style.visibility == 'visible'? 'hidden' : 'visible';
+    
+    document.getElementById('wetle').style.visibility=    document.getElementById('wetle').style.visibility == 'visible'? 'hidden' : 'visible';
+
+    
 }
+
